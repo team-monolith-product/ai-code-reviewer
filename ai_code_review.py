90e996e6333d96bfa95551d931aeab07236ebaa4
이창환 <lch@team-mono.com>
2025-04-28 17:22:42 +0900

    fix: 스레드 사용자 이름 변수명 수정

--- ai_code_review.py
+++ ai_code_review.py
@@ -417,8 +417,8 @@ def build_prompt(
     for _, threads in id_to_threads.items():
         thread_summary = []
         for thread in threads:
-            nameOrLogin = thread.user.get('name', thread.user.login)
-            thread_summary.append(f"From: {nameOrLogin}\n" f"{thread.body}\n")
+            name_or_login = thread.user.get('name', thread.user.login)
+            thread_summary.append(f"From: {name_or_login}\n" f"{thread.body}\n")
         comments_summary.append(
             f"Thread At {threads[0].path}:L{threads[0].position}\n"
             + "--------------\n".join(thread_summary)
